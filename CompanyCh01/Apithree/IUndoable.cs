743d21b9e1d7567733ccc9637210b8a1b738d8c9
wsx\aaron <1481539483@qq.com>
2020-05-18 16:26:39 +0800

    新增了在子类中重新实现接口

---
+++ CompanyCh01/Apithree/IUndoable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apithree
+{
+    /* 开始定义接口，来用虚方法实现接口成员。
+     * 1. 首先先定义了接口,没有返回值的方法。
+     * 2. 定义了一个类,继承了我们这个接口,然后我们在这个类下定义了一个方法,但是我们想让以后的类，继承我后，可以更改我下边的方法,然后我就加了一个virtual修饰符，说明别人继承了我这个类,可以重构我这个方法
+     * 3. 我们又定义了一个类，继承了我们首先定义的类,然后又定义了一个方法,因为我们想重构我派生(继承)的类下边方法,所以必须要加入override这个修饰符,然后进行了重构。
+     * 4. 显式实现的接口成员不能标识为virtual，也不能实现通常意义的重写，但是它可以被重新实现
+     */
+    interface IUndoable { void Undo(); }
+    public class TextBox : IUndoable
+    {
+        public virtual void Undo() => Console.WriteLine("TextBOx.Undo");
+    }
+    public class RichTextBox : TextBox
+    {
+        public override void Undo() => Console.WriteLine("RichTextBox.Undo");
+
+
+    }
+}
---
+++ CompanyCh01/Apithree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Apithree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. 不管从基类还是接口中调用接口成员，调用的都是子类的实现,我们可以从下边这几个调用方法看出来。 
+             * 2. 实例化了类，让richTextBox拥有了RichTextBox它的权限。 
+             * 3. 然后，通过richTextBox这个方法调用了.Undo看一下输出的是什么
+             * 4. 再通过继承的基类,调用Undo方法看看输出是什么
+             * 5. 再通过接口，调用undo方法看看输出是什么
+             * 6. 结论就是他们的输出都是RichTextBox.Und。
+             * 7. 重点重点：显式实现的接口成员不能标识为virtual，也不能实现我们这种方法的重写，但是它可以被重新实现。
+             */
+            RichTextBox richTextBox = new RichTextBox();
+            richTextBox.Undo();
+            ((TextBox)richTextBox).Undo();
+            ((IUndoable)richTextBox).Undo();
+
+
+        }
+    }
+}
