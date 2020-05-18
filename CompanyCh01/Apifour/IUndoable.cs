f40262411f5fcac46a633ece3b625e7c3c7bc408
wsx\aaron <1481539483@qq.com>
2020-05-18 17:54:32 +0800

    还是写的接口重新修改代码

---
+++ CompanyCh01/Apifour/IUndoable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apifour
+{
+    interface IUndoable { void Undo(); }
+    public class TextBox : IUndoable
+    {
+        void IUndoable.Undo() => Undo();
+        //void IUndoable.Undo() => Console.WriteLine("显示实现方法Undo");
+        //public void Undo() => Console.WriteLine("实现了TextBox");
+        protected virtual void Undo() => Console.WriteLine("实现了TextBox");
+    }
+    public class ReadTextBox : TextBox, IUndoable
+    {
+        //public new void Undo() => Console.WriteLine("实现了ReadTextBox");
+        protected override void Undo() => Console.WriteLine("实现了ReadTextBox");
+    }
+}
---
+++ CompanyCh01/Apifour/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Apifour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadTextBox readTextBox = new ReadTextBox();
+            //readTextBox.Undo();
+            ((IUndoable)readTextBox).Undo();
+            //((TextBox)readTextBox).Undo();
+        }
+    }
+}
